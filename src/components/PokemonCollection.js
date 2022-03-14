4840866d21fcddf29a21d01ead6a3c33d2ae0285
Julius Garrido <jflgarrido@gmail.com>
2022-03-14 17:04:18 -0600

    in progress

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,11 +1,23 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
+import { useEffect } from "react/cjs/react.production.min";
 
-function PokemonCollection() {
+function PokemonCollection({cards, setCards}) {
+  
+
+  const pokeCards = cards.map((pokemon) => {
+    return <PokemonCard
+      key={pokemon.id}
+      id={pokemon.id}
+      name={pokemon.name}
+      hp={pokemon.hp}
+      image={pokemon.sprites.front}
+      />
+  })
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokeCards}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [cards, setCards] = useState([])
+  useEffect(()=> {
+    fetch("http://localhost:3001/pokemon")
+    .then(r => r.json())
+    .then(data => setCards(data))
+  }, [])
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +19,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection cards={cards} setCards={setCards} />
     </Container>
   );
 }
